d869fd1986e14de455605e477ff3e77741ffaf97
kgachuki25 <kgachuki@syr.edu>
2025-02-19 20:49:00 -0500

    process_file.py completed

--- code/process_file.py
+++ code/process_file.py
@@ -4,3 +4,32 @@ You should output the parsed package and total package size for each package in 
 
 Screenshot available as process_file.png
 '''
+import streamlit as st
+from io import StringIO
+import json
+from packaging import parse_packaging, calc_total_units, get_unit
+
+st.title("Process File of Packages")
+
+packFile = st.file_uploader("Upload package file:", type = "txt")
+
+if packFile:
+    packBin = packFile.getvalue()
+    packText = StringIO(packBin.decode("utf-8")).read()
+    all_packages = [] # to store each line parsed for json output
+    for line in packText.split("\n"):
+        pack_list = parse_packaging(line)
+        total_size = calc_total_units(pack_list)
+        unit = get_unit(pack_list)
+        all_packages.append(pack_list)
+
+        st.info(f"{line} -> Total Package Size: {total_size} {unit}")
+
+    fname = packFile.name
+    jsonFile = fname.replace(".txt", ".json")
+    with open(jsonFile, "w") as j:
+        json.dump(all_packages, j, indent = 4)
+    st.success(f"{len(all_packages)} written to {jsonFile}")
+
+
+
